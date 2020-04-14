41d7a499ed1377ac78bb0104111cf3d3f8977bdd
Abhinish098 <44517818+Abhinish098@users.noreply.github.com>
2020-04-14 18:02:02 +0530

    Minor

---
+++ main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+function App()
+{
+  return <div>Hello, World</div>
+}
+
+ReactDOM.render(App.
+  document.getElementsByClassName('root')
+)
