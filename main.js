e316e93fe404939a6605258a567bd4b54d021f24
Abhinish098 <44517818+Abhinish098@users.noreply.github.com>
2020-04-17 19:51:58 +0530

    Login

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-function App()
-{
-  return <div>Hello, World</div>
-}
-
-ReactDOM.render(App.
-  document.getElementsByClassName('root')
-)
+(function ($) {
+    $(function () {
+        $('nav ul li > a:not(:only-child)').click(function (e) {
+            $(this).siblings('.nav-dropdown').toggle();
+            $('.nav-dropdown').not($(this).siblings()).hide();
+            e.stopPropagation();
+        });
+        $('html').click(function () {
+            $('.nav-dropdown').hide();
+        });
+    });
+    document.querySelector('#nav-toggle').addEventListener('click', function () {
+        this.classList.toggle('active');
+    });
+    $('#nav-toggle').click(function () {
+        $('nav ul').toggle();
+    });
+})(jQuery);
